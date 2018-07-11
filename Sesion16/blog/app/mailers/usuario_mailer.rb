25f3b6d7ab8aeb17541625407ab89dfcfcaa3d3e
JaiderFernandez <JaiderF@gmail.com>
2018-07-10 20:59:51 -0500

    Sesion 16, envío correos usando ruby

--- Sesion16/blog/app/mailers/usuario_mailer.rb
+++ Sesion16/blog/app/mailers/usuario_mailer.rb
@@ -1,9 +1,9 @@
 class UsuarioMailer < ApplicationMailer
     def bienvenidos_email
         @usuario = params[:usuario]
-        @url = 'http://localhost:3000/users/sign_in'
+        @url = 'http://localhost:3000/users/sign_in/'
 
         attachments['Welcome.jpg'] = File.read('C:\\Users\\TopGroup\\Documents\\OERD\\Sesion16\\blog\\app\\assets\\images\\Welcome.jpg')
-        mail(to: @usuario.email, subject: 'Bienvenido al sitio')
+        mail(to: @usuario.email, subject: 'Bienvenido al sitio de prueba')
     end
 end
